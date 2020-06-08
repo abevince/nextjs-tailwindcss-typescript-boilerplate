89fb2cf54cb76623bffae97492bdc6b0e00948f1
Vincent Abesamis <vincentabesamis03@gmail.com>
2020-06-08 17:39:12 +0800

    NextJS, TailwindCSS, Tailwind UI and TypeScript biolerplate

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndexPage: React.FC = () => {
+  return (
+    <div className="max-w-full min-h-screen flex justify-center items-center">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default IndexPage
